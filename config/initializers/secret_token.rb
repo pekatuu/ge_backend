d72b4793b4086d39c3004892088b86ead14b853c
pekatuu <pekatuu@local.host>
2013-03-21 21:50:16 +0900

    add initial files and jQueryGantt as a submodule

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeBackend::Application.config.secret_token = '26593448596b5a9910e0c25a58d8bbcdbd80c6774deaba098447d870960c781676225291ed745ae75cbffd70e6428328bd353ad3918c361ae0e3c585588e1036'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GeBackend::Application.config.session_store :cookie_store, key: '_ge_backend_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GeBackend::Application.config.session_store :active_record_store
